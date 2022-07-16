1016dbce33b6a41d1e9290e8d6b1172fca675f2c
Jose Marcilio <tpmarc@gmail.com>
2022-07-16 00:15:51 -0300

    Removed unnecessary code

--- packages/@crispy:contentful/index.ts
+++ packages/@crispy:contentful/index.ts
@@ -1,24 +1,5 @@
-import { ContentfulClientApi, createClient, Entry } from "contentful";
-import { PageService, IPageService } from "./page";
+import { ContentfulClientApi, createClient } from "contentful";
 export { documentToHtmlString } from "@contentful/rich-text-html-renderer";
-
-let client: ContentfulClientApi;
-
-export const withClient = (
-  content: (client: ContentfulClientApi) => IPageService
-) => {
-  if (!client) {
-    client = createClient({
-      space: process.env.CONTENTFUL_SPACE_ID!,
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
-    });
-  }
-
-  return content(client);
-};
-
-export const getPageService = () => withClient(PageService);
-
 export class ContentfulClient {
   private static _instance: ContentfulClientApi;
 
