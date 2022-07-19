d6cf108815f8e800945cdec15e386c76d6dc7eae
Jose Marcilio <tpmarc@gmail.com>
2022-07-18 22:47:15 -0300

    Added tailwind

--- apps/web/components/Navigation.tsx
+++ apps/web/components/Navigation.tsx
@@ -2,16 +2,22 @@ import Link from "next/link";
 
 export function Navigation() {
   return (
-    <nav className="p-2 font-bold underline">
-      <ul className="flex gap-4">
-        <li>
-          <Link href="/">Index</Link>
+    <nav className="font-black">
+      <ul className="flex items-center gap-2 select-none">
+        <li className="flex items-center justify-center rounded-lg hover:bg-black hover:bg-opacity-30 transition-all">
+          <Link href="/" passHref>
+            <a className="px-4 p-2">Articles</a>
+          </Link>
         </li>
-        <li>
-          <Link href="/my-first-page">First</Link>
+        <li className="flex items-center justify-center rounded-lg hover:bg-black hover:bg-opacity-30 transition-all">
+          <Link href="/my-first-page" passHref>
+            <a className="px-4 p-2">Books</a>
+          </Link>
         </li>
-        <li>
-          <Link href="/my-second-page">Second</Link>
+        <li className="flex items-center justify-center rounded-lg hover:bg-black hover:bg-opacity-30 transition-all">
+          <Link href="/my-second-page" passHref>
+            <a className="px-4 p-2">Workshops</a>
+          </Link>
         </li>
       </ul>
     </nav>
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -7,19 +7,41 @@ import {
 import { AppProps } from "next/app";
 import { Navigation } from "../components/Navigation";
 
+import "@fontsource/eczar";
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Link from "next/link";
 
 const variants = {
-  hidden: { opacity: 0, x: -200 },
+  hidden: { opacity: 0, x: -100 },
   enter: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: 200 },
+  exit: { opacity: 0, x: 100 },
 };
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <LazyMotion features={domAnimation}>
-      <Navigation />
+      <header className="flex items-center px-8 p-4 bg-orange-600 text-2xl text-white">
+        <div className="container flex p-4 gap-6">
+          <h1 className="h-full font-black text-4xl">Logo</h1>
+
+          <Navigation />
+        </div>
+      </header>
+
+      <ul className="flex px-2 gap-3 font-black text-orange-700 bg-white shadow-xl shadow-gray-200">
+        <li className="px-0 p-8">
+          <Link href="/tags" passHref>
+            <a className="px-6 p-4 bg-orange-100 rounded-lg">React</a>
+          </Link>
+        </li>
+
+        <li className="px-0 p-8">
+          <Link href="/tags" passHref>
+            <a className="px-6 p-4 bg-orange-100 rounded-lg">CSS</a>
+          </Link>
+        </li>
+      </ul>
 
       <AnimatePresence exitBeforeEnter>
         <motion.div
